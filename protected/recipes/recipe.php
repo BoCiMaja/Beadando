cf06a791457593e9d52d84fe0e5516ca69cee859
Maja Bódi <buksi.pajti@gmail.com>
2020-05-11 23:37:51 +0200

    Recipe módosítása 1

--- protected/recipes/recipe.php
+++ protected/recipes/recipe.php
@@ -2,7 +2,7 @@
 	<h1>Page access is forbidden!</h1>
 <?php else : ?>
 	<?php 
-		$query = "SELECT id, dish_name, difficulty, category, calorie, ingredients, leiras FROM recipes WHERE id = :id";
+		$query = "SELECT id, dish_name, difficulty, category, calorie, ingredients, directions, description FROM recipes WHERE id = :id";
 		require_once DATABASE_CONTROLLER;
 		$params = [':id' => $_GET['id']];
 		$recipes = getList($query, $params);
@@ -10,9 +10,15 @@
 
 	<?php foreach ($recipes as $r) : ?>
 		<h1> <?=$r['dish_name']?></h1>
-		<h3><?=$w['email']?></h3>
-		<?= $w['gender'] == 0 ? 'Female' : ($w['gender'] == 1 ? 'Male' : 'Other'); ?> </br>
-		<?=$w['nationality']?>
+		<h5><?=$r['category']?></h5>
+		<?=$r['description']?> <br>
+		<?= $r['difficulty'] == 1 ? 'Very Easy' : ($r['difficulty'] == 2 ? 'Easy' : ($r['difficulty'] == 3 ? 'Medium' : ($r['difficulty'] == 4 ? 'Hard' : 'Very Hard')));?> | <?=$r['calorie']?> kcal
+		<hr style="width: 75%">
+		<h5>Ingredients</h5>
+		<?=$r['ingredients']?>
+		<hr style="width: 75%">
+		<h5>Direction</h5>
+		<?=$r['directions']?>
 	<?php endforeach; ?>
 
 <?php endif; ?>
