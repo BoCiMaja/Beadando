73a970916b2829b5c775c19b2c6ade39f27762e2
Maja Bódi <buksi.pajti@gmail.com>
2020-05-12 15:25:43 +0200

    User list módosítása 1

--- protected/user/user_list.php
+++ protected/user/user_list.php
@@ -1,8 +1,23 @@
 <?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] < 1) : ?>
 	<h1>Page access is forbidden!</h1>
 <?php else : ?>
+
 	<?php 
-	$query = "SELECT first_name, last_name, email, permission FROM users";
+		if(array_key_exists('d', $_GET) && !empty($_GET['d'])) {
+			$query = "DELETE FROM users WHERE id = :id";
+			$params = [':id' => $_GET['d']];
+			require_once DATABASE_CONTROLLER;
+			if($_GET['d'] == $_SESSION['uid']){
+				echo "You can't delete yourself in here!";
+			} else if(!executeDML($query, $params)) {
+				echo "Error during deleting user!";
+			}
+			
+		}
+	?>
+
+	<?php 
+	$query = "SELECT id, first_name, last_name, email, permission FROM users";
 	require_once DATABASE_CONTROLLER;
 	$users = getList($query);
 	?>
@@ -17,6 +32,8 @@
 					<th scope="col">Last Name</th>
 					<th scope="col">Email</th>
 					<th scope="col">Permission</th>
+					<th scope="col">Edit</th>
+					<th scope="col">Delete</th>
 				<tr>
 			</thead>
 			<tbody>
@@ -29,6 +46,8 @@
 						<td><?=$u['last_name'] ?></td>
 						<td><?=$u['email'] ?></td>
 						<td><?=$u['permission'] ?></td>
+						<td><a href="<?='index.php?P=edit_user&e='.$u['id']?>">Edit</a></td>
+						<td><a href="?P=users&d=<?=$u['id'] ?>">Delete</a></td>
 					</tr>
 				<?php endforeach; ?>
 			</tbody>
