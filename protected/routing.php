dda18acf474901674b7fd21188d7bd3691e5b74a
Maja Bódi <buksi.pajti@gmail.com>
2020-05-12 15:26:04 +0200

    Routing módosítása 1

--- protected/routing.php
+++ protected/routing.php
@@ -15,12 +15,12 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
 
 
 		case 'login': !isUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
-
 		case 'register': !isUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
+		case 'logout': isUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
 
-		case 'logout': isUserLoggedIn() ? UserLogout() : header('Location: inndex.php'); break;
 
 		case 'users': isUserLoggedIn() ? require_once PROTECTED_DIR.'user/user_list.php' : header('Location: inndex.php'); break;
+		case 'edit_user': isUserLoggedIn() ? require_once PROTECTED_DIR.'user/edit_user.php' : header('Location: index.php'); break;
 
 		default: require_once PROTECTED_DIR.'normal/404.php'; break;
 	}
