d5d16b08481f4377707dec4580b5126fef671cae
Maja Bódi <buksi.pajti@gmail.com>
2020-05-12 11:10:43 +0200

    Add módisítása 2

--- protected/recipes/add.php
+++ protected/recipes/add.php
@@ -1,4 +1,4 @@
-<?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] <= 1) : ?>
+<?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] < 1) : ?>
 	<h1>Page access is forbidden!</h1>
 <?php else : ?>
 	<?php
