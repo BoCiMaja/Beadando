778ab212ca85614ea6e4a396bcb042765c82743b
Maja Bódi <buksi.pajti@gmail.com>
2020-05-14 11:54:23 +0200

    Register módosítása 1

--- protected/user/register.php
+++ protected/user/register.php
@@ -10,17 +10,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])){
   ];
 
   if(empty($postData['fname']) || empty($postData['lname']) || empty($postData['email']) || empty($postData['email1']) || empty($postData['password']) || empty($postData['password1'])) {
-    echo "Hiányzó adat(ok)!";
+    echo "Missing data(s)!";
   } else if($postData['email'] != $postData['email1']) {
-    echo "Az email címek nem egyeznek!";
+    echo "Email adresses must match!";
   } else if(!filter_var($postData['email'], FILTER_VALIDATE_EMAIL)){
-    echo "Hibás email formátum!";
+    echo "Invalid email format!";
   } else if($postData['password'] != $postData['password1']){
-    echo "A jelszavak nem egyeznek!";
+    echo "Passwords must match!";
   } else if(strlen($postData['password']) < 6) {
-    echo "A jelszó túl rövid! Legalább 6 karakter hosszúnak kell lennie";
+    echo "The password is too short! The password needs to be at least 6 characters.";
   } else if(!UserRegister($postData['email'], $postData['password'], $postData['fname'], $postData['lname'])) {
-    echo "Sikertelen regisztráció!";
+    echo "Registration failed!";
   }
 
   $postData['password'] = $postData['password1'] = "";
